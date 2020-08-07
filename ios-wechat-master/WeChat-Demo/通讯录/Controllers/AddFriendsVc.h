25386cc8bb2f51bfebc912ec2fdc913f28d3c6ee
kk177- <564886232@qq.com>
2020-08-07 10:41:47 +0800

    fix/yjx/通讯录文件缺失

---
+++ ios-wechat-master/WeChat-Demo/通讯录/Controllers/AddFriendsVc.h
@@ -0,0 +1,18 @@
+//
+//  AddFriendsVc.h
+//  WeChat-Demo
+//
+//  Created by fu00 on 2020/8/5.
+//  Copyright © 2020 mac os. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SDAutoLayout.h>
+#import <JMessage/JMessage.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddFriendsVc : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
